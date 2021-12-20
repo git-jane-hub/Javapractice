9428ea689c753bc8798e9bde41b07b7d67cdeff3
jane <choiwodls@naver.com>
2021-12-20 23:28:04 +0900

    calendar class2

--- javabasic/src/ch10/Ex10_04.java
+++ javabasic/src/ch10/Ex10_04.java
@@ -6,11 +6,11 @@ import java.util.Calendar;
 public class Ex10_04 {
 	public static void main(String[] args) {
 		// 날짜 지정
-		Calendar date1 = Calendar.getInstance();
-		date1.set(2002, 10, 20);
-//		date1.set(Calendar.YEAR, 2021);
-//		date1.set(Calendar.MONTH, 11);
-//		date1.set(Calendar.DATE, 19);
+		Calendar date0 = Calendar.getInstance();
+		date0.set(2002, 10, 20);
+//		date0.set(Calendar.YEAR, 2021);
+//		date0.set(Calendar.MONTH, 11);
+//		date0.set(Calendar.DATE, 19);
 		
 		// 시간 지정 
 		Calendar time1 = Calendar.getInstance();
@@ -18,5 +18,28 @@ public class Ex10_04 {
 		time1.set(Calendar.MINUTE, 32);
 		time1.set(Calendar.SECOND, 30);
 		
+		// 요일은 1부터 시작, DAY_OF_WEEK[0]는 비워둠
+		final String[] DAY_OF_WEEK = {"", "일", "월", "화", "수", "목", "금", "토"};
+		
+		// Calendar 객체 생성
+		Calendar date1 = Calendar.getInstance();	// 오늘과 차이를 얻으려는 날짜 
+		Calendar date2 = Calendar.getInstance();	// 오늘 
+		
+		// 월은 0부터 시작하기 때문에 12월인 경우, 11로 지정해야함
+		// date1.set(2021, Calendar.DECEMBER, 20)으로 할 수 있음
+		date1.set(2021, 0, 1);	// 2021년 1월 1일로 날짜 지정
+		System.out.println("date1은 " + toString(date1) + DAY_OF_WEEK[date1.get(Calendar.DAY_OF_WEEK)] + "요일이고, ");
+		System.out.println("오늘(date2)은 " + toString(date2) + DAY_OF_WEEK[date1.get(Calendar.DAY_OF_WEEK)] + "요일입니다.");
+	
+		// 두 날짜 간 차이를 얻으려면, getTimeInMillis() 천분의 일초 단위로 변환해야함
+		long difference = (date2.getTimeInMillis() - date1.getTimeInMillis()) / 1000;
+		System.out.println(date2.getTimeInMillis());
+		System.out.println(date1.getTimeInMillis());
+		System.out.println("그 날(date1)부터 지금(date2)까지 " + difference + "초가 지났습니다.");
+		System.out.println("일(day)로 계산하면 " + difference / (24 * 60 * 60) + "일 입니다.");	// 1일 = 24 * 60 * 60
+		
+	}
+	public static String toString(Calendar date) {
+		return date.get(Calendar.YEAR) + "년 " + (date.get(Calendar.MONTH) + 1)+ "월 " + date.get(Calendar.DATE) + "일 ";
 	}
 }
--- javabasic/src/ch10/Ex10_05.java
+++ javabasic/src/ch10/Ex10_05.java
@@ -4,10 +4,33 @@ import java.util.Calendar;
 
 public class Ex10_05 {
 	public static void main(String[] args) {
-		// 요일은 1부터 시작, DAY_OF_WEEK[0]는 비워둠
-		final String[] DAY_OF_WEEK = {"", "일", "월", "화", "수", "목", "금", "토"};
+		final int[] TIME_UNIT = {3600, 60, 1};	// 큰 단위를 앞에 놓음
+		final String[] TIME_UNIT_NAME = {"시간 ", "분 ", "초 "};
 		
-		Calendar date1 = Calendar.getInstance();
-		Calendar date2 = Calendar.getInstance();
+		Calendar time1 = Calendar.getInstance();
+		Calendar time2 = Calendar.getInstance();
+		
+		// 10시 20분 30초 
+		time1.set(Calendar.HOUR_OF_DAY, 10);
+		time1.set(Calendar.MINUTE, 20);
+		time1.set(Calendar.SECOND, 30);
+		
+		// 20시 30분 10초
+		time2.set(Calendar.HOUR_OF_DAY, 20);
+		time2.set(Calendar.MINUTE, 30);
+		time2.set(Calendar.SECOND, 10);
+		
+		System.out.println("time1 : " + time1.get(Calendar.HOUR_OF_DAY) + "시 " + time1.get(Calendar.MINUTE) + "분 " + time1.get(Calendar.SECOND) + "초");
+		System.out.println("time2 : " + time2.get(Calendar.HOUR_OF_DAY) + "시 " + time2.get(Calendar.MINUTE) + "분 " + time2.get(Calendar.SECOND) + "초");
+		
+		long difference = Math.abs(time2.getTimeInMillis() - time1.getTimeInMillis()) / 1000;
+		System.out.println("time1과 time2의 차이는 " + difference + "초 입니다.");
+		
+		String tmp = "";
+		for(int i = 0; i < TIME_UNIT.length; i++) {
+			tmp += difference / TIME_UNIT[i] + TIME_UNIT_NAME[i];
+			difference %= TIME_UNIT[i];
+		}
+		System.out.println("시분초로 변환하면 " + tmp + "입니다.");
 	}
 }
